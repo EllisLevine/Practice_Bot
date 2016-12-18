b5cbe94539665e05a905986fccbdcfe4472174e1
EllisLevine <emlevine5@gmail.com>
2016-12-17 20:38:51 -0500

    First commit

---
+++ src/org/usfirst/frc/team86/robot/Kicker.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team86.robot;
+
+public class Kicker {
+	
+	public static final int KICK_STOPPED = 0;
+	public static final int KICK_ATTACK = 1;
+	public static final int KICK_BACK = -1;
+	
+
+}
---
+++ src/org/usfirst/frc/team86/robot/SetValDrive.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team86.robot;
+
+public enum SetValDrive {
+	ARCADE_DRIVE;
+}
